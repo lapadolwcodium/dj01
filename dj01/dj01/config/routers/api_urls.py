2b0317e7cda5189ae21a512270effe07eecb25ae
ton <lapadol.w@codium.co>
2017-11-24 19:09:52 +0700

    api get without paramiter values and return json

--- dj01/dj01/config/routers/api_urls.py
+++ dj01/dj01/config/routers/api_urls.py
@@ -1,12 +1,14 @@
 from rest_framework import routers
+from django.conf.urls import url
 
 router = routers.DefaultRouter()
 # Add your API router here. It should be sorted by alphabet.
 
+from dj01.config.logic.test_api import take_json
 
 # For function API view
 urlpatterns = [
-
+ url(r'^testapi/', take_json, name="export-order")
 ]
 
 urlpatterns += router.urls
